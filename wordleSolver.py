525404dff9ae7ddd07c9e7cb9002b0557297a42c
miguel-m-pena <pena.m.miguel@gmail.com>
2022-02-06 12:01:44 +0000

    Added a check in check_correct() for no guess having been made. Refactored repeated code into the char_pos_frequency() method and restructured how values are bound to variables. Refactored layout to work from a top-down approach of main method and helper methods.

--- wordleSolver.py
+++ wordleSolver.py
@@ -42,58 +42,91 @@ class Website:
         pos = 0
         for word in bs_text.findAll('game-tile'):
             letter = word.get('letter')
-            eval = word.get('evaluation')
-            results[pos] = (letter, eval)
+            evaluation = word.get('evaluation')
+            results[pos] = (letter, evaluation)
             pos += 1
 
         return self.counter - 1, results
 
 
+def play():
+    web = Website()
+    information = generate_starting_possibilities()
+    start_word = input("Enter a word. Use ! if you do not wish to enter a word.")
+    if start_word == "!":
+        output = guess(web, information)
+    else:
+        output = web.send_word_get_answer(start_word)
+        for value in output[1].values():
+            if None in value:
+                web.background.send_keys(Keys.BACKSPACE)
+                web.background.send_keys(Keys.BACKSPACE)
+                web.background.send_keys(Keys.BACKSPACE)
+                web.background.send_keys(Keys.BACKSPACE)
+                web.background.send_keys(Keys.BACKSPACE)
+                print("Invalid word. Defaulting to optimal guessing.")
+                output = guess(web, information)
+                break
+            else:
+                web.counter += 1
+                print(output[1])
+                output = output[1]
+                break
+    while (not check_correct(web, output)) and web.counter < 6:
+        information = update_info(information[0], output)
+        output = guess(web, information)
+    print("Congratulations!")
+    time.sleep(3)
+    web.browser.quit()
+
+
 def generate_starting_possibilities():
     word_list = words.raw('en').split("\n")
     five_letter_words = []
-    first_char, second_char, third_char, fourth_char, fifth_char = [], [], [], [], []
     for word in word_list:
         if len(word) == 5:
             word = word.lower()
             five_letter_words.append(word)
-            first_char.append(word[0])
-            second_char.append(word[1])
-            third_char.append(word[2])
-            fourth_char.append(word[3])
-            fifth_char.append(word[4])
-    return (five_letter_words, Counter(first_char), Counter(second_char), Counter(third_char),
-            Counter(fourth_char), Counter(fifth_char))
+    return char_pos_frequency(five_letter_words)
 
 
-def guess(web, five_letter_words, first_letter_count, second_letter_count, third_letter_count, \
-          fourth_letter_count, fifth_letter_count):
+def char_pos_frequency(word_list):
+    first_char, second_char, third_char, fourth_char, fifth_char = [], [], [], [], []
+    for word in word_list:
+        first_char.append(word[0])
+        second_char.append(word[1])
+        third_char.append(word[2])
+        fourth_char.append(word[3])
+        fifth_char.append(word[4])
+    return word_list, Counter(first_char), Counter(second_char), Counter(third_char), Counter(fourth_char), Counter(fifth_char)
+
+
+def guess(web, information):
+    five_letter_words = information[0]
+    first_letter_count = information[1]
+    second_letter_count = information[2]
+    third_letter_count = information[3]
+    fourth_letter_count = information[4]
+    fifth_letter_count = information[5]
     for i in range(len(first_letter_count)):
         for j in range(len(second_letter_count)):
             for k in range(len(third_letter_count)):
                 for l in range(len(fourth_letter_count)):
                     for m in range(len(fifth_letter_count)):
-                        guess = first_letter_count.most_common()[i][0] + second_letter_count.most_common()[j][0] + \
-                                third_letter_count.most_common()[k][0] + fourth_letter_count.most_common()[l][0] + \
-                                fifth_letter_count.most_common()[m][0]
-                        # print(guess)
-                        if guess in five_letter_words:
-                            output = web.send_word_get_answer(guess)
+                        guess_word = first_letter_count.most_common()[i][0] + \
+                                     second_letter_count.most_common()[j][0] + third_letter_count.most_common()[k][0] +\
+                                     fourth_letter_count.most_common()[l][0] + fifth_letter_count.most_common()[m][0]
+                        if guess_word in five_letter_words:
+                            output = web.send_word_get_answer(guess_word)
                         else:
                             continue
 
                         for value in output[1].values():
                             if None in value:
                                 web.background.send_keys(Keys.BACKSPACE)
-                                web.background.send_keys(Keys.BACKSPACE)
-                                web.background.send_keys(Keys.BACKSPACE)
-                                web.background.send_keys(Keys.BACKSPACE)
-                                web.background.send_keys(Keys.BACKSPACE)
-                                break
                             else:
                                 web.counter += 1
                                 print(output[1])
-                                # print("fuck")
                                 return output[1]
 
 
@@ -136,58 +169,16 @@ def update_info(five_letter_words, output):
                     if word[key] != output[key][0]:
                         updated_words.remove(word)
             five_letter_words = copy.deepcopy(updated_words)
-    # print(f'Here {five_letter_words}')
-    first_char, second_char, third_char, fourth_char, fifth_char = [], [], [], [], []
-    for word in five_letter_words:
-        first_char.append(word[0])
-        second_char.append(word[1])
-        third_char.append(word[2])
-        fourth_char.append(word[3])
-        fifth_char.append(word[4])
-    """    total_letter_count = Counter(" ".join(five_letter_words))
-    total_letter_count.pop(' ')"""
-    return (five_letter_words, Counter(first_char), Counter(second_char), Counter(third_char),
-            Counter(fourth_char), Counter(fifth_char))
+    return char_pos_frequency(five_letter_words)
 
-def check_correct(output):
-    return all("correct" == val[1] for val in output.values())
 
-def play():
-    web = Website()
-    five_letter_words, first_letter_count, second_letter_count, third_letter_count, \
-    fourth_letter_count, fifth_letter_count = generate_starting_possibilities()
-    start_word = input("Enter a word. Use ! if you do not wish to enter a word.")
-    if start_word == "!":
-        output = guess(web, five_letter_words, first_letter_count, second_letter_count,
-                       third_letter_count, \
-                       fourth_letter_count, fifth_letter_count)
+def check_correct(web, output):
+    if output:
+        return all("correct" == val[1] for val in output.values())
     else:
-        output = web.send_word_get_answer(start_word)
-        for value in output[1].values():
-            if None in value:
-                web.background.send_keys(Keys.BACKSPACE)
-                web.background.send_keys(Keys.BACKSPACE)
-                web.background.send_keys(Keys.BACKSPACE)
-                web.background.send_keys(Keys.BACKSPACE)
-                web.background.send_keys(Keys.BACKSPACE)
-                print("Invalid word. Defaulting to optimal guessing.")
-                output = guess(web, five_letter_words, first_letter_count, second_letter_count,
-                               third_letter_count, \
-                               fourth_letter_count, fifth_letter_count)
-                break
-            else:
-                web.counter += 1
-                print(output[1])
-                output = output[1]
-                break
-    while (not check_correct(output)) and web.counter < 6:
-        five_letter_words, first_letter_count, second_letter_count, third_letter_count, \
-            fourth_letter_count, fifth_letter_count = update_info(five_letter_words, output)
-        output = guess(web, five_letter_words, first_letter_count, second_letter_count,
-                       third_letter_count, fourth_letter_count, fifth_letter_count)
-    print("Congratulations!")
-    time.sleep(3)
-    web.browser.quit()
+        print("No possible guesses identified")
+        web.browser.quit()
 
 
-play()
+if __name__ == "__main__":
+    play()
